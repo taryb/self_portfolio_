274aa1d064acf82511a603b456e9c903f65ac1ea
taryb <tbounavong@gmail.com>
2023-01-07 20:23:15 -0800

    fixed light n dark font

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -28,14 +28,15 @@ export default {
 	>
 		<!-- Banner left contents -->
 		<div class="w-full md:w-1/3 text-left">
-			<p>Hi, my name is </p>
+			<p 	class="font-general-semibold text-center sm:text-left text-ternary-dark dark:text-primary-light"
+>Hi, my name is </p>
 			<h1
 				class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light"
 			>
 				Tary Bounavong
 			</h1>
-			<h2>I build things for fun.</h2>
-			<h3>I am currently working as an</h3>
+			<h2 class="font-general-semibold text-center sm:text-left text-ternary-dark dark:text-primary-light">I build things for fun.</h2>
+			<h3 class="font-general-semibold text-center sm:text-left text-ternary-dark dark:text-primary-light">I am currently working as an</h3>
 			<p
 				class="font-general-medium mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left leading-none text-gray-400"
 			>

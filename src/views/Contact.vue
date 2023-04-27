52fae0b6e90515dc600ad4a6cb709f6550ad66e7
taryb <tbounavong@gmail.com>
2023-04-27 12:54:45 -0700

    remove contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,9 @@
 <script>
 import feather from 'feather-icons';
-import ContactForm from '@/components/contact/ContactForm.vue';
 import ContactDetails from '@/components/contact/ContactDetails.vue';
 
 export default {
 	components: {
-		ContactForm,
 		ContactDetails,
 	},
 	data: () => {
@@ -38,8 +36,6 @@ export default {
 	<div
 		class="container mx-auto flex flex-col-reverse md:flex-row py-5 md:py-10 md:mt-10"
 	>
-		<!-- Contact form -->
-		<ContactForm />
 
 		<!-- Contact details -->
 		<ContactDetails :contacts="contacts" />

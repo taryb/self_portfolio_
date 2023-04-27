ba229f187167d4e566638e4572b77ddff9bcf8b3
taryb <tbounavong@gmail.com>
2023-04-27 12:35:47 -0700

    deleted the download resume and updated the info

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -40,25 +40,8 @@ export default {
 			<p
 				class="font-general-medium mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left leading-none text-gray-400"
 			>
-				X-Ray Optic Engineer I
+				X-Ray Optic Engineer I, working to be a Software Engineer/Data Analyst
 			</p>
-			<div class="flex justify-center sm:block">
-				<a
-					download="tb_oldresume.pdf"
-					href="/public/files/tb_oldresume.pdf"
-					class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
-					aria-label="Download Resume"
-				>
-					<i
-						data-feather="arrow-down-circle"
-						class="ml-0 sm:ml-1 mr-2 sm:mr-3 w-5 sm:w-6 duration-100"
-					></i>
-					<span
-						class="text-sm sm:text-lg font-general-medium duration-100"
-						>Download CV</span
-					></a
-				>
-			</div>
 		</div>
 
 		<!-- Banner right illustration -->

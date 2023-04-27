e350a4f50eba4e83a2425813b3938c18a78a25c2
taryb <tbounavong@gmail.com>
2023-04-27 12:43:07 -0700

    had some errors, wen t through and deleted the button and hireme stuff

--- src/components/shared/AppHeader.vue
+++ src/components/shared/AppHeader.vue
@@ -2,15 +2,11 @@
 import ThemeSwitcher from '../ThemeSwitcher';
 import feather from 'feather-icons';
 import AppHeaderLinks from './AppHeaderLinks.vue';
-import Button from '../reusable/Button.vue';
 
 export default {
 	components: {
 		ThemeSwitcher,
-		HireMeModal,
-		AppHeaderLinks,
-		Button,
-	},
+		AppHeaderLinks,	},
 	data() {
 		return {
 			isOpen: false,
@@ -106,33 +102,6 @@ export default {
 					class="block sm:hidden bg-ternary-light dark:bg-ternary-dark hover:bg-hover-light dark:hover:bg-hover-dark hover:shadow-sm px-2.5 py-2 rounded-lg"
 				/>
 
-				<!-- Small screen hamburger menu -->
-				<div class="sm:hidden">
-					<button
-						@click="isOpen = !isOpen"
-						type="button"
-						class="focus:outline-none"
-						aria-label="Hamburger Menu"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							class="h-7 w-7 fill-current text-secondary-dark dark:text-ternary-light"
-						>
-							<path
-								v-if="isOpen"
-								fill-rule="evenodd"
-								d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-								clip-rule="evenodd"
-							></path>
-							<path
-								v-if="!isOpen"
-								fill-rule="evenodd"
-								d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-							></path>
-						</svg>
-					</button>
-				</div>
 			</div>
 
 			<!-- Header links -->

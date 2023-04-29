21f5e6857cb6d5da79cd3fb6ab5ddfe288aeffe7
taryb <tbounavong@gmail.com>
2023-04-29 00:23:55 -0700

    fixed syntax

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -72,9 +72,7 @@ export default {
 							'Web Scraping Libraries: Libraries such as BeautifulSoup, Scrapy, Selenium, and PyQuery are widely used to facilitate web scraping tasks.',
 							'Web Development Tools: You may have used web development tools like Inspect Element to identify the HTML elements containing the data you want to scrape.',
 							'Data Storage Tools: After extracting data from websites, you may have stored it in different formats such as CSV, Excel, or JSON files, or in databases like MySQL or MongoDB.',
-							'Cloud Computing Tools: You may have also leveraged cloud computing tools like Amazon Web Services or Google Cloud Platform to run your web scraping tasks more efficiently.
-
-',
+							'Cloud Computing Tools: You may have also leveraged cloud computing tools like Amazon Web Services or Google Cloud Platform to run your web scraping tasks more efficiently.',
 							
 						],
 					},
@@ -84,8 +82,7 @@ export default {
 					{
 						id: 1,
 						details:
-							' As someone passionate about data, I recognized the power of web scraping in collecting valuable information from websites. I researched different tools and techniques, focusing on popular Python libraries like BeautifulSoup and Scrapy, which are widely used in the web scraping community.I then embarked on a hands-on learning journey, practicing web scraping on a variety of websites to extract the desired data using Python code. This process involved identifying the HTML elements containing the data, writing code to extract it, and storing it in a format for analysis. Throughout my learning process, I faced various challenges, such as anti-scraping measures and complex website structures. However, I persevered, leveraging online resources such as forums and documentation to overcome these obstacles. As I gained more experience, I explored more advanced techniques such as APIs and handling large datasets. I also saw the potential of web scraping beyond data collection, such as building predictive models or informing business decisions. Overall, my process of learning about web scraping involved research, experimentation, and problem-solving. Through dedication and persistence, I have become proficient in this powerful tool for data analysis, and I am excited to apply my skills in various real-world contexts.',
-				
+							' As someone passionate about data, I recognized the power of web scraping in collecting valuable information from websites. I researched different tools and techniques, focusing on popular Python libraries like BeautifulSoup and Scrapy, which are widely used in the web scraping community.I then embarked on a hands-on learning journey, practicing web scraping on a variety of websites to extract the desired data using Python code. This process involved identifying the HTML elements containing the data, writing code to extract it, and storing it in a format for analysis. Throughout my learning process, I faced various challenges, such as anti-scraping measures and complex website structures. However, I persevered, leveraging online resources such as forums and documentation to overcome these obstacles. As I gained more experience, I explored more advanced techniques such as APIs and handling large datasets. I also saw the potential of web scraping beyond data collection, such as building predictive models or informing business decisions. Overall, my process of learning about web scraping involved research, experimentation, and problem-solving. Through dedication and persistence, I have become proficient in this powerful tool for data analysis, and I am excited to apply my skills in various real-world contexts.',},
 				],
 				socialSharingsHeading: 'Share This',
 				socialSharings: [

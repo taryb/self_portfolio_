512c7dacb69080c8ba3a827711a70c6bab124229
taryb <tbounavong@gmail.com>
2023-04-29 00:19:56 -0700

    delete twitterusr

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -68,12 +68,14 @@ export default {
 					{
 						title: 'Tools & Technologies',
 						techs: [
-							'HTML',
-							'CSS',
-							'JavaScript',
-							'Laravel',
-							'TailwindCSS',
-							'MySql',
+							'Python: This is a popular programming language that is commonly used for web scraping. You may have used Python libraries like BeautifulSoup and Scrapy to extract data from websites.',
+							'Web Scraping Libraries: Libraries such as BeautifulSoup, Scrapy, Selenium, and PyQuery are widely used to facilitate web scraping tasks.',
+							'Web Development Tools: You may have used web development tools like Inspect Element to identify the HTML elements containing the data you want to scrape.',
+							'Data Storage Tools: After extracting data from websites, you may have stored it in different formats such as CSV, Excel, or JSON files, or in databases like MySQL or MongoDB.',
+							'Cloud Computing Tools: You may have also leveraged cloud computing tools like Amazon Web Services or Google Cloud Platform to run your web scraping tasks more efficiently.
+
+',
+							
 						],
 					},
 				],
@@ -82,8 +84,7 @@ export default {
 					{
 						id: 1,
 						details:
-							'found itof Cambodia’s young population enter the labor force each year. Labor force participation among the working age population (15–64 years) increased from 7.7 million in 2009 to 8.3 million in 2015. While the country is endowed with an ample supply of labor, the labor force is still characterized by very low levels of education and skill. The average educational attainment of the current workforce is primary school completion (grade 6) or below—a major barrier to industrial diversification efforts and moving up the value chain. The 2015 Cambodia Socio-Economic Survey found that nearly half (46%) of the labor force have either not completed primary education or never attended school. Less than 7% of workers completed high school (grade 12). There is a pressing need to respond to the growing demand for an adaptable workforce with professional skills and proper workplace behavior. In response, the government has taken steps to reform and strengthen the education and training system, including an enhanced role of the private sector in skills development and providing incentives to enterprises to train their own workforce. The pilot Skills Development Fund (SDF) is an industry-driven workforce development program. The general notion is to build confidence in industry partnership and to engage more strongly with the private sector and their training needs. Firms willing but at times hesitant to further develop their workforce may be supported by the SDF to further invest in workforce development. Motivated private workforce development initiatives and intentions shall be mainly supported by the SDF (which in turn consists of a motivated, skilled, and client-oriented team).',
-					}
+							' As someone passionate about data, I recognized the power of web scraping in collecting valuable information from websites. I researched different tools and techniques, focusing on popular Python libraries like BeautifulSoup and Scrapy, which are widely used in the web scraping community.I then embarked on a hands-on learning journey, practicing web scraping on a variety of websites to extract the desired data using Python code. This process involved identifying the HTML elements containing the data, writing code to extract it, and storing it in a format for analysis. Throughout my learning process, I faced various challenges, such as anti-scraping measures and complex website structures. However, I persevered, leveraging online resources such as forums and documentation to overcome these obstacles. As I gained more experience, I explored more advanced techniques such as APIs and handling large datasets. I also saw the potential of web scraping beyond data collection, such as building predictive models or informing business decisions. Overall, my process of learning about web scraping involved research, experimentation, and problem-solving. Through dedication and persistence, I have become proficient in this powerful tool for data analysis, and I am excited to apply my skills in various real-world contexts.',
 				
 				],
 				socialSharingsHeading: 'Share This',
@@ -92,7 +93,7 @@ export default {
 						id: 1,
 						name: 'Twitter',
 						icon: 'twitter',
-						url: 'https://twitter.com/ChhinghorDev',
+						url: 'https://twitter.com',
 					},
 					{
 						id: 2,
